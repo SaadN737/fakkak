965dfb4cab3b65899062d17ed47cd122e0ba06a7
SaadN737 <saad7374567@gmail.com>
2026-02-07 22:39:26 +0200

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const { YoutubeTranscript } = require('youtube-transcript');
+
+module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    const { videoId } = req.query;
+
+    if (!videoId) {
+        return res.status(400).json({ error: 'فين الـ videoId يا وحش؟' });
+    }
+
+    try {
+        const transcript = await YoutubeTranscript.fetchTranscript(videoId);
+        const fullText = transcript.map(t => t.text).join(' ');
+        res.status(200).json({ status: 'success', data: fullText });
+    } catch (error) {
+        res.status(500).json({ status: 'error', message: error.message });
+    }
+};
